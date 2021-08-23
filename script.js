480422e70ae46ab25af153340b455d358fe88b2d
tams sokari <github@tamssokari.com>
2021-08-23 13:11:00 +0100

    enh: add `getSiteURL` function, and work relative to siteURL

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 'use strict';
 
+function getSiteURL() {
+	var currentScriptSrc = document.currentScript.src;
+	const qualifiedURL = currentScriptSrc.split('/script.js')[0];
+
+	return qualifiedURL;
+}
+
+const config = {
+	siteURL: getSiteURL()
+};
+
 function createIframe() {
 	var iframe = document.createElement('iframe');
 
-	iframe.setAttribute('src', '/iframe.html');
+	iframe.setAttribute('src', `${config.siteURL}/iframe.html`);
 	iframe.setAttribute('id', 'iframe');
+	iframe.setAttribute('name', 'smile-identity-hosted-integration');
+	iframe.setAttribute('frameborder', '0');
+	iframe.setAttribute('allow', 'camera; geolocation; encrypted-media;');
+	iframe.setAttribute('sandbox', 'allow-forms allow-same-origin allow-scripts');
+	iframe.setAttribute('allowtransparency', 'true');
+
 	iframe.style.width = '100vw';
 	iframe.style.height = '100vh';
 

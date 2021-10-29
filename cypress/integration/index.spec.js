afffb6982b87f9498fdbdc94ec5774fd94c3156d
tams sokari <github@tamssokari.com>
2021-10-29 13:26:42 +0100

    tests: use new id for iframe

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -2,7 +2,7 @@ it('should load an iframe', () => {
 	cy.visit('/');
 
 	cy
-		.get('#iframe')
+		.get('#smile-identity-hosted-integration')
 		.its('0.contentDocument.body')
 		.should('be.visible')
 });

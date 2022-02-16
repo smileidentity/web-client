513d5dc2e2529cc21d410b686476f8d493454733
tams sokari <github@tamssokari.com>
2022-02-16 13:49:26 +0100

    enh: add comment explaining additional endpoint entry

--- src/js/ekyc-smartselfie.js
+++ src/js/ekyc-smartselfie.js
@@ -1,9 +1,11 @@
 var eKYCSmartSelfie = function eKYCSmartSelfie() {
 	'use strict';
 
+	// NOTE: In order to support prior integrations, we have `live` and
+	// `production` pointing to the same URL
 	const endpoints = {
 		'sandbox': 'https://testapi.smileidentity.com/v1',
-        'live': 'https://api.smileidentity.com/v1',
+		'live': 'https://api.smileidentity.com/v1',
 		'production': 'https://api.smileidentity.com/v1'
 	}
 

ed8515eebe8149fbe8c5a1f625d2ee05b49d3589
Laud Bruce Tagoe <antmanlay1@gmail.com>
2023-02-01 13:19:19 +0000

    Exclude Document Verification from restricted ID types selection.

--- src/js/doc-verification.js
+++ src/js/doc-verification.js
@@ -4,17 +4,15 @@ var documentVerification = function documentVerification() {
 	// NOTE: In order to support prior integrations, we have `live` and
 	// `production` pointing to the same URL
 	const endpoints = {
-		sandbox: 'https://testapi.smileidentity.com/v1',
-		qa: "https://devapi.smileidentity.com/v1",
-		live: 'https://api.smileidentity.com/v1',
-		production: 'https://api.smileidentity.com/v1'
+		'sandbox': 'https://testapi.smileidentity.com/v1',
+		'live': 'https://api.smileidentity.com/v1',
+		'production': 'https://api.smileidentity.com/v1'
 	}
 
 	var config;
 	var activeScreen;
 	var id_info, images, partner_params;
 	var productConstraints;
-	var partnerProductConstraints;
 
 	var SelectIDType = document.querySelector('#select-id-type');
 	var SmartCameraWeb = document.querySelector('smart-camera-web');
@@ -28,46 +26,12 @@ var documentVerification = function documentVerification() {
 
 	var fileToUpload, uploadURL;
 
-	function postData(url = '', data = {}) {
-		return fetch(url, {
-			method: 'POST',
-			mode: 'cors',
-			cache: 'no-cache',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(data)
-		});
-	}
-
 	async function getProductConstraints() {
 		try {
-			const productsConfigPayload = {
-				partner_id: config.partner_details.partner_id,
-				token: config.token,
-				partner_params
-			}
-	
-			const productsConfigUrl = `${endpoints[config.environment]}/products_config`;
-			const productsConfigPromise = postData(productsConfigUrl, productsConfigPayload);
-			const servicesPromise = fetch(`${endpoints[config.environment]}/services`);
-			const [productsConfigResponse, servicesResponse] = await Promise.all([
-				productsConfigPromise,
-				servicesPromise
-			])
+			const response = await fetch(`${endpoints[config.environment]}/services`);
+			const json = await response.json();
 
-			if (productsConfigResponse.ok && servicesResponse.ok) {
-				const partnerConstraints = await productsConfigResponse.json()
-				const generalConstraints = await servicesResponse.json()
-
-				return {
-					partnerConstraints,
-					generalConstraints: generalConstraints.hosted_web['doc_verification']
-				}
-			} else {
-				throw new Error("Failed to get supported ID types");
-			}
+			return json.hosted_web['doc_verification'];
 		} catch (e) {
 			throw new Error("Failed to get supported ID types", { cause: e });
 		}
@@ -79,11 +43,9 @@ var documentVerification = function documentVerification() {
 			activeScreen = SelectIDType;
 
 			try {
+				productConstraints = await getProductConstraints();
+				initializeSession(productConstraints);
 				getPartnerParams();
-				const { partnerConstraints, generalConstraints } = await getProductConstraints();
-				partnerProductConstraints = partnerConstraints;
-				productConstraints = generalConstraints;
-				initializeSession(generalConstraints, partnerConstraints);
 
 				localStorage.setItem('SmileIdentityConfig', event.data);
 			} catch (e) {
@@ -92,11 +54,11 @@ var documentVerification = function documentVerification() {
 		}
 	}, false);
 
-	function initializeSession(generalConstraints, partnerConstraints) {
-		const supportedCountries = Object.keys(generalConstraints)
+	function initializeSession(constraints) {
+		const supportedCountries = Object.keys(constraints)
 			.map(countryCode => ({
 				code: countryCode,
-				name: generalConstraints[countryCode].name
+				name: constraints[countryCode].name
 			})).sort((a, b) => {
 				if (a.name < b.name) {
 					return -1;
@@ -117,18 +79,15 @@ var documentVerification = function documentVerification() {
 			validCountries = supportedCountries.filter(value =>
 				Object.keys(config.id_selection).includes(value));
 		} else {
-			validCountries = Object.keys(partnerConstraints.idSelection.doc_verification);
+			validCountries = supportedCountries;
 		}
 
 		// ACTION: Load Countries as <option>s
 		validCountries.forEach(country => {
-			const countryObject = generalConstraints[country]
-			if (countryObject) {
-				const option = document.createElement('option');
-				option.setAttribute('value', country);
-				option.textContent = countryObject.name;
-				selectCountry.appendChild(option);
-			}
+			const option = document.createElement('option');
+			option.setAttribute('value', country);
+			option.textContent = constraints[country].name;
+			selectCountry.appendChild(option);
 		});
 
 		// ACTION: Enable Country Selection
@@ -136,9 +95,12 @@ var documentVerification = function documentVerification() {
 
 		selectCountry.addEventListener('change', e => {
 			if (e.target.value) {
-				const validIDTypes = config.id_selection ? config.id_selection : partnerConstraints.idSelection.doc_verification;
-				const constrainedIDTypes = Object.keys(generalConstraints[e.target.value].id_types);
-				const selectedIDTypes = validIDTypes[e.target.value].filter(value => constrainedIDTypes.includes(value))
+				let validIDTypes = [];
+				const constrainedIDTypes = Object.keys(constraints[e.target.value].id_types);
+
+				let selectedIDTypes = config.id_selection ?
+					config.id_selection[e.target.value].filter(value => constrainedIDTypes.includes(value)) :
+					constrainedIDTypes;
 
 				// ACTION: Reset ID Type <select>
 				selectIDType.innerHTML = '';
@@ -147,7 +109,7 @@ var documentVerification = function documentVerification() {
 				selectedIDTypes.forEach(IDType => {
 					const option = document.createElement('option');
 					option.setAttribute('value', IDType);
-					option.textContent = generalConstraints[e.target.value]['id_types'][IDType].label;
+					option.textContent = constraints[e.target.value]['id_types'][IDType].label;
 					selectIDType.appendChild(option);
 				});
 
@@ -318,11 +280,21 @@ var documentVerification = function documentVerification() {
 			partner_params
 		}
 
-		// const URL = `${endpoints[config.environment] || config.environment}/upload`;
-		// this is just for QA and will be removed before deployment
-		const URL = 'https://testapi.smileidentity.com/v1/upload'
+		const fetchConfig = {
+			cache: 'no-cache',
+			mode: 'cors',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			method: 'POST',
+			body: JSON.stringify(payload),
+		};
+
+		const URL = `${endpoints[config.environment] || config.environment}/upload`;
+
 		try {
-			const response = await postData(URL, payload);
+			const response = await fetch(URL, fetchConfig);
 			const json = await response.json();
 
 			if (json.error) throw new Error(json.error);

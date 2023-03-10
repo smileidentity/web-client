e9c06f8c46e7a9e66f110327d0c2f203fd7cce45
tams sokari <github@tamssokari.com>
2023-03-10 15:07:26 +0100

    enh: add pre-selection logic for document verification

--- cypress/tests/id-selection.cy.js
+++ cypress/tests/id-selection.cy.js
@@ -109,6 +109,87 @@ describe('No ID Selection', () => {
 			.should('contain', 'Bank Verification');
 	});
 
+	it('document_verification', () => {
+		cy.visit('/document-verification');
+
+		cy
+			.intercept({
+				method: 'POST',
+				url: '*upload*'
+			}, {
+				upload_url: "https://smile-uploads-development01.s3.us-west-2.amazonaws.com/videos/212/212-0000060103-0gdzke3mdtlco5k0sdfh6vifzcrd3n/ekyc_smartselfie.zip"
+			}).as('getUploadURL');
+
+		cy
+			.intercept({
+				method: 'PUT',
+				url: 'https://smile-uploads-development01.s3.us-west-2.amazonaws.com/videos/212/212-0000060103-0gdzke3mdtlco5k0sdfh6vifzcrd3n/ekyc_smartselfie.zip'
+			}, {
+				statusCode: 200
+			}).as('successfulUpload');
+
+		cy
+			.selectNINIDType()
+
+		cy
+			.navigateThroughCameraScreens();
+
+		cy
+			.getIFrameBody()
+			.find('smart-camera-web')
+			.invoke('attr', 'document-type')
+			.should('eq', 'NIN')
+
+		cy
+			.getIFrameBody()
+			.find('smart-camera-web')
+			.shadow()
+			.find('#id-camera-screen')
+			.should('be.visible');
+
+		cy
+			.getIFrameBody()
+			.find('smart-camera-web')
+			.shadow()
+			.find('#capture-id-image')
+			.click();
+
+		cy
+			.wait(2000);
+
+		cy
+			.getIFrameBody()
+			.find('smart-camera-web')
+			.shadow()
+			.find('#id-camera-screen')
+			.should('not.be.visible');
+
+		cy
+			.getIFrameBody()
+			.find('smart-camera-web')
+			.shadow()
+			.find('#select-id-image')
+			.click();
+
+		cy
+			.getIFrameBody()
+			.find('smart-camera-web')
+			.shadow()
+			.find('#id-review-screen')
+			.should('not.be.visible');
+
+		cy
+			.wait('@getUploadURL');
+
+		cy
+			.wait('@successfulUpload');
+
+		cy
+			.getIFrameBody()
+			.find('#complete-screen')
+			.should('be.visible');
+	});
+
 	it('enhanced_kyc', () => {
 		cy.visit('/enhanced_kyc');
 
@@ -295,6 +376,102 @@ describe('Preselected Country', () => {
 			.should('be.visible')
 			.should('contain', 'Nigeria')
 			.should('contain', 'Bank Verification');
+	});
+
+	it('document_verification', () => {
+		cy.visit('/document-verification-pre-select-country');
+
+		cy
+			.intercept({
+				method: 'POST',
+				url: '*upload*'
+			}, {
+				upload_url: "https://smile-uploads-development01.s3.us-west-2.amazonaws.com/videos/212/212-0000060103-0gdzke3mdtlco5k0sdfh6vifzcrd3n/ekyc_smartselfie.zip"
+			}).as('getUploadURL');
+
+		cy
+			.intercept({
+				method: 'PUT',
+				url: 'https://smile-uploads-development01.s3.us-west-2.amazonaws.com/videos/212/212-0000060103-0gdzke3mdtlco5k0sdfh6vifzcrd3n/ekyc_smartselfie.zip'
+			}, {
+				statusCode: 200
+			}).as('successfulUpload');
+
+		cy
+			.loadIDOptions();
+
+		cy
+			.getIFrameBody()
+			.find('#country')
+			.should('contain', 'Nigeria')
+
+		cy
+			.getIFrameBody()
+			.find('#id_type')
+			.select('PASSPORT')
+
+		cy
+			.getIFrameBody()
+			.find('#submitConfig')
+			.click();
+
+		cy
+			.navigateThroughCameraScreens();
+
+		cy
+			.getIFrameBody()
+			.find('smart-camera-web')
+			.invoke('attr', 'document-type')
+			.should('eq', 'PASSPORT')
+
+		cy
+			.getIFrameBody()
+			.find('smart-camera-web')
+			.shadow()
+			.find('#id-camera-screen')
+			.should('be.visible');
+
+		cy
+			.getIFrameBody()
+			.find('smart-camera-web')
+			.shadow()
+			.find('#capture-id-image')
+			.click();
+
+		cy
+			.wait(2000);
+
+		cy
+			.getIFrameBody()
+			.find('smart-camera-web')
+			.shadow()
+			.find('#id-camera-screen')
+			.should('not.be.visible');
+
+		cy
+			.getIFrameBody()
+			.find('smart-camera-web')
+			.shadow()
+			.find('#select-id-image')
+			.click();
+
+		cy
+			.getIFrameBody()
+			.find('smart-camera-web')
+			.shadow()
+			.find('#id-review-screen')
+			.should('not.be.visible');
+
+		cy
+			.wait('@getUploadURL');
+
+		cy
+			.wait('@successfulUpload');
+
+		cy
+			.getIFrameBody()
+			.find('#complete-screen')
+			.should('be.visible');
 	});
 
 	it('enhanced_kyc', () => {
@@ -490,6 +667,98 @@ describe('Preselected Country and ID Type', () => {
 			.should('be.visible')
 			.should('contain', 'Nigeria')
 			.should('contain', 'Bank Verification');
+	});
+
+	it('document_verification', () => {
+		cy.visit('/document-verification-pre-select-id-type');
+
+		cy
+			.intercept({
+				method: 'POST',
+				url: '*upload*'
+			}, {
+				upload_url: "https://smile-uploads-development01.s3.us-west-2.amazonaws.com/videos/212/212-0000060103-0gdzke3mdtlco5k0sdfh6vifzcrd3n/ekyc_smartselfie.zip"
+			}).as('getUploadURL');
+
+		cy
+			.intercept({
+				method: 'PUT',
+				url: 'https://smile-uploads-development01.s3.us-west-2.amazonaws.com/videos/212/212-0000060103-0gdzke3mdtlco5k0sdfh6vifzcrd3n/ekyc_smartselfie.zip'
+			}, {
+				statusCode: 200
+			}).as('successfulUpload');
+
+		cy.loadIDOptions();
+
+		cy.getIFrameBody()
+			.find('#country')
+			.should('not.be.visible');
+
+		cy.getIFrameBody()
+			.find('#id_type')
+			.should('not.be.visible');
+
+		cy.getIFrameBody()
+			.find('smart-camera-web')
+			.should('be.visible');
+
+		cy
+			.navigateThroughCameraScreens();
+
+		cy
+			.getIFrameBody()
+			.find('smart-camera-web')
+			.invoke('attr', 'document-type')
+			.should('eq', 'PASSPORT')
+
+		cy
+			.getIFrameBody()
+			.find('smart-camera-web')
+			.shadow()
+			.find('#id-camera-screen')
+			.should('be.visible');
+
+		cy
+			.getIFrameBody()
+			.find('smart-camera-web')
+			.shadow()
+			.find('#capture-id-image')
+			.click();
+
+		cy
+			.wait(2000);
+
+		cy
+			.getIFrameBody()
+			.find('smart-camera-web')
+			.shadow()
+			.find('#id-camera-screen')
+			.should('not.be.visible');
+
+		cy
+			.getIFrameBody()
+			.find('smart-camera-web')
+			.shadow()
+			.find('#select-id-image')
+			.click();
+
+		cy
+			.getIFrameBody()
+			.find('smart-camera-web')
+			.shadow()
+			.find('#id-review-screen')
+			.should('not.be.visible');
+
+		cy
+			.wait('@getUploadURL');
+
+		cy
+			.wait('@successfulUpload');
+
+		cy
+			.getIFrameBody()
+			.find('#complete-screen')
+			.should('be.visible');
 	});
 
 	it('enhanced_kyc', () => {

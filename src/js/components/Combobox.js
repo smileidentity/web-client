24bd5bcf2f8a1b7c5d1ba1e87d8a6b1893117ec1
tams sokari <tams@smileidentity.com>
2023-09-26 15:50:49 -0400

    refactor: remove search icon from Combobox

--- src/js/components/Combobox.js
+++ src/js/components/Combobox.js
@@ -81,9 +81,6 @@ class ComboboxTrigger extends HTMLElement {
 		this.innerHTML = `${this.type === "text" ?
 			`
 				<div>
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
-						<path stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m22 22-2-2m-8.5 1a9.5 9.5 0 1 0 0-19 9.5 9.5 0 0 0 0 19Z"/>
-					</svg>
 					<input ${this.value ? `value="${this.value}" ` : ''}${this.disabled ? ' disabled ' : ''}type="text" placeholder="${this.label}" />
 					<button ${this.disabled ? 'disabled ' : ''}tabindex='-1' type='button'>
 						<span class="visually-hidden">Toggle</span>

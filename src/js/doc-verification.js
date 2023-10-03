361f11a3b1a27cb7d81d28e42d6402d0dbaa6aba
Michael D'Angelo <michael.l.dangelo@gmail.com>
2023-10-03 12:41:02 -0400

    move parseJWT into common

--- src/js/doc-verification.js
+++ src/js/doc-verification.js
@@ -1,5 +1,5 @@
 const JSZip = require("jszip");
-const { getEndpoint } = require("./common");
+const { getEndpoint, parseJWT } = require("./common");
 
 (function documentVerification() {
   "use strict";
@@ -274,36 +274,7 @@ const { getEndpoint } = require("./common");
   });
 
   function getPartnerParams() {
-    function parseJWT(token) {
-      /**
-       * A JSON Web Token (JWT) uses a base64 URL encoded string in it's body.
-       *
-       * in order to get a regular JSON string, we would follow these steps:
-       *
-       * 1. get the body of a JWT string
-       * 2. replace the base64 URL delimiters ( - and _ ) with regular URL delimiters ( + and / )
-       * 3. convert the regular base64 string to a string
-       * 4. encode the string from above as a URIComponent,
-       *    ref: just above this - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent#examples
-       * 5. decode the URI Component to a JSON string
-       * 6. parse the JSON string to a javascript object
-       */
-      const base64Url = token.split(".")[1];
-      const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-      const jsonPayload = decodeURIComponent(
-        atob(base64)
-          .split("")
-          .map(function (c) {
-            return `%${c.charCodeAt(0).toString(16)}`;
-          })
-          .join(""),
-      );
-
-      return JSON.parse(jsonPayload);
-    }
-
     const { partner_params: partnerParams } = parseJWT(config.token);
-
     partner_params = { ...partnerParams, ...(config.partner_params || {}) };
   }
 

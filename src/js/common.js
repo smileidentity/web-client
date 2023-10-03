676658608b7de7704e0c2764720574b68cbe0eab
Michael <michael.l.dangelo@gmail.com>
2023-10-03 16:51:03 -0400

    Update src/js/common.js
    Co-authored-by: tams sokari <tamssokari@users.noreply.github.com>

--- src/js/common.js
+++ src/js/common.js
@@ -10,7 +10,6 @@
  */
 const getEndpoint = (environment) =>
   ({
-    development: "https://devapi.smileidentity.com/v1",
     sandbox: "https://testapi.smileidentity.com/v1",
     live: "https://api.smileidentity.com/v1",
     production: "https://api.smileidentity.com/v1",

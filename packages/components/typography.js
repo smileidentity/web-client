3d8de6fd7994360309ff0061964ba38fb9c43226
Barnabas Nsoh <banasco@gmail.com>
2023-12-15 15:55:26 +0000

    Checkin typography and styles files

---
+++ packages/components/typography.js
@@ -0,0 +1,30 @@
+const typography = `
+.h2 {
+	font-size: 1rem;
+	font-style: normal;
+	font-weight: 700;
+	line-height: 1.5rem;
+  }
+
+  .h4 {
+	font-size: 1rem;
+	font-style: normal;
+	font-weight: 400;
+	line-height: 1.5rem;
+  }
+
+  .p {
+	font-size: 0.875rem;
+	font-style: normal;
+	font-weight: 400;
+	line-height: 1.125rem;
+  }
+  .p2 {
+	font-size: 1rem;
+	font-style: normal;
+	font-weight: 500;
+	line-height: 1rem;
+  }
+`
+
+export default typography;

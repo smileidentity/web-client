b96fcdc0dd27a2e53b76c73a4b72512e6f45cc52
Barnabas Nsoh <banasco@gmail.com>
2024-01-19 13:22:46 +0000

    Add selfie capture screen

---
+++ packages/components/selfie-capture/SelfieCapture.stories.js
@@ -0,0 +1,15 @@
+import "./SelfieCapture";
+
+const meta = {
+    component: "selfie-capture",
+};
+
+export default meta;
+
+export const SelfieCapture = {
+    render: () => `
+        <selfie-capture
+        >
+        </selfie-capture>
+    `,
+}

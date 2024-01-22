bf998169165fcf5a1c4b653347195c39faed7730
Barnabas Nsoh <banasco@gmail.com>
2024-01-22 16:16:54 +0000

    Update typography

--- packages/components/typography.js
+++ packages/components/typography.js
@@ -1,29 +1,27 @@
 const typography = `
-.h2 {
-	font-size: 1rem;
-	font-style: normal;
-	font-weight: 700;
-	line-height: 1.5rem;
-  }
-
-  .h4 {
-	font-size: 1rem;
-	font-style: normal;
-	font-weight: 400;
-	line-height: 1.5rem;
-  }
-
-  .p {
+  .text-sm {
 	font-size: 0.875rem;
 	font-style: normal;
 	font-weight: 400;
 	line-height: 1.125rem;
   }
-  .p2 {
+  .text-base {
 	font-size: 1rem;
 	font-style: normal;
 	font-weight: 500;
 	line-height: 1rem;
+  }
+  .font-bold {
+	font-weight: 700;
+  }
+  .font-semibold {
+	font-weight: 600;
+  }
+  .font-medium {
+	font-weight: 500;
+  }
+  .font-normal {
+	font-weight: 400;
   }
 `
 

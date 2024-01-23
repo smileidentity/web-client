74d0874a06c69ea619d41761d39e80c0bfddea71
Barnabas A Nsoh <banasco@gmail.com>
2024-01-23 10:41:21 +0000

    [ehn] Refactor SmartCamera and SmartFileUpload (#151)
    * Add selfi instruction screen
    
    * reformat
    
    * Remove unused methods
    
    * Add Document instruction template
    
    * Add document instruction
    
    * show new document entry screen
    
    * Handle take photo butotn click
    
    * Add refactored components
    
    * Handle file upload
    Handle back navigation in back of id
    
    * Allow skipping document capture
    
    * Rename hide to skip
    
    * Update tests
    
    * Update accept and retry buttons in id review page
    
    * Update camera display
    
    * Remove border in id camera and review pages
    Add text button to review
    Update image height in review
    
    * Add note about glare in review screens
    
    * Add id review component
    Fix DocumentInstruction story
    
    * Use new component, IdReview in smart camera web
    Fix review image display
    
    * Fix recapture
    
    * Update smart camera tests
    
    * Support capture modes in document-instruction
    
    * Update smart web camera tests
    
    * Refactor id capture and instruction screens to new directories
    
    * Refactor id capture and instruction screens to new directories
    
    * remove duplicate screen
    
    * Set stream in document capture component
    Update video preview to capture more context
    Add styles and typography
    
    * Revert changes
    
    * Checkin typography and styles files
    
    * Update capture button margin
    
    * Capture and review document
    
    * Add router to manage routes
    
    * Add loader when checking permission
    
    * Remove console log
    
    * Set stream to null after stopping all tracks
    Add thank you screen
    Add capture back id
    Add hide back capture
    
    * add story for hiding both instruction and back of id
    
    * Remove router: fix activeScreen getting reset to null
    
    * Move selfie capture component into a new dir
    Rename instructions to document-instructions
    Add base styles to document-capture component
    
    * Update error color
    
    * Add upload error
    Fix file selector
    Copy fileupload class to components
    
    * Maintain only smart camera and upload
    Move document capture to new branch
    
    * Move selfie instruction to new branch
    
    * Remove document-capture exports from package.json
    
    * revert width
    
    * Update typography
    
    * Move camera error handler to SmartCamera
    
    ---------
    
    Co-authored-by: Ed Fricker <888909+beastawakens@users.noreply.github.com>

---
+++ packages/components/typography.js
@@ -0,0 +1,28 @@
+const typography = `
+  .text-sm {
+	font-size: 0.875rem;
+	font-style: normal;
+	font-weight: 400;
+	line-height: 1.125rem;
+  }
+  .text-base {
+	font-size: 1rem;
+	font-style: normal;
+	font-weight: 500;
+	line-height: 1rem;
+  }
+  .font-bold {
+	font-weight: 700;
+  }
+  .font-semibold {
+	font-weight: 600;
+  }
+  .font-medium {
+	font-weight: 500;
+  }
+  .font-normal {
+	font-weight: 400;
+  }
+`
+
+export default typography;

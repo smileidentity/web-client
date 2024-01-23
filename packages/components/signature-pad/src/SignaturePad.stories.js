46366550b530db8d29cd2649e68a5bb8aa926783
tams sokari <tams@smileidentity.com>
2024-01-23 20:24:22 +0100

    stories: add storybook page for SignaturePad component

---
+++ packages/components/signature-pad/src/SignaturePad.stories.js
@@ -0,0 +1,24 @@
+import "./SignaturePad";
+
+const meta = {
+    component: "smileid-signature-pad",
+};
+
+export default meta;
+
+export const SignaturePad = {
+    render: () => `
+        <smileid-signature-pad
+        >
+        </smileid-signature-pad>
+    `,
+}
+
+export const SignaturePadWithUploads = {
+    render: () => `
+        <smileid-signature-pad
+          allow-upload
+        >
+        </smileid-signature-pad>
+    `,
+}

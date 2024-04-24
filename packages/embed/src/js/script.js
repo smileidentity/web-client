69baf1ebabf00202e629328f00ab4c407d7d09ec
Chris DeLauder <chris@smileidentity.com>
2024-04-24 16:41:10 -0700

    replace localhost with dev

--- packages/embed/src/js/script.js
+++ packages/embed/src/js/script.js
@@ -9,7 +9,7 @@ Sentry.init({
   // Performance Monitoring
   tracesSampleRate: 0.05, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ['localhost', /^https:\/\/links\.usesmilid\.com/, /^https:\/\/links\.sandbox\.usesmilid\.com/],
+  tracePropagationTargets: [/^https:\/\/links\.usesmilid\.com/, /^https:\/\/links\.sandbox\.usesmilid\.com/, /^https:\/\/links\.dev\.usesmilid\.com/],
   // Session Replay
   replaysSessionSampleRate: 0.01, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.

54a91f214b898d8fd012ebaa611a0c1b7102a154
Barnabas Nsoh <banasco@gmail.com>
2024-05-03 14:25:17 +0000

    Add dev endpoint to url

--- packages/embed/src/js/enhanced-document-verification.js
+++ packages/embed/src/js/enhanced-document-verification.js
@@ -7,6 +7,7 @@ import { version as sdkVersion } from '../../package.json';
   // NOTE: In order to support prior integrations, we have `live` and
   // `production` pointing to the same URL
   const endpoints = {
+    development: 'https://devapi.smileidentity.com/v1', // TODO: Remove this endpoint before merging
     sandbox: 'https://testapi.smileidentity.com/v1',
     live: 'https://api.smileidentity.com/v1',
     production: 'https://api.smileidentity.com/v1',

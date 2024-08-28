c494046e4302279d5012c0b0886a6339d5c39fde
Barnabas Nsoh <banasco@gmail.com>
2024-08-28 18:47:41 +0000

    fix lint

--- packages/smart-camera-web/src/index.js
+++ packages/smart-camera-web/src/index.js
@@ -1,9 +1,12 @@
 import PoweredBySmileId from './PoweredBySmileId';
 import SmartCameraWeb from './SmartCameraWeb';
 
-if (window.customElements && !window.customElements.get('powered-by-smile-id')) {
-   window.customElements.define('powered-by-smile-id', PoweredBySmileId);
+if (
+  window.customElements &&
+  !window.customElements.get('powered-by-smile-id')
+) {
+  window.customElements.define('powered-by-smile-id', PoweredBySmileId);
 }
 if (window.customElements && !window.customElements.get('smart-camera-web')) {
-   window.customElements.define('smart-camera-web', SmartCameraWeb);
+  window.customElements.define('smart-camera-web', SmartCameraWeb);
 }

540e55f87c33149a99350a25f4d348b2167f166a
Barnabas Nsoh <banasco@gmail.com>
2024-09-05 15:37:06 +0000

    add test

--- packages/web-components/cypress/e2e/smart-camera-web-agent-mode.cy.js
+++ packages/web-components/cypress/e2e/smart-camera-web-agent-mode.cy.js
@@ -2,8 +2,13 @@ context('SmartCameraWeb', () => {
   beforeEach(() => {
     cy.visit('/smart-camera-web-agent-mode');
   });
+
   it('should switch from the selfie mode to agent mode"', () => {
     cy.log('Enable agent mode for tests');
+    cy.get('smart-camera-web')
+      .shadow()
+      .find('selfie-capture')
+      .invoke('attr', 'allow-agent-mode', 'true');
     cy.get('smart-camera-web')
       .shadow()
       .find('selfie-capture')
@@ -53,6 +58,59 @@ context('SmartCameraWeb', () => {
       .find('selfie-capture')
       .shadow()
       .should('contain.text', 'Agent Mode On');
+
+    cy.get('smart-camera-web')
+      .shadow()
+      .find('selfie-capture')
+      .shadow()
+      .should('contain.text', 'Take a Selfie');
+
+    cy.get('smart-camera-web')
+      .shadow()
+      .find('selfie-capture')
+      .shadow()
+      .find('#start-image-capture')
+      .click();
+
+    cy.tick(8000);
+
+    cy.get('smart-camera-web')
+      .shadow()
+      .find('selfie-capture')
+      .shadow()
+      .should('not.be.visible');
+
+    cy.get('smart-camera-web')
+      .shadow()
+      .find('selfie-capture-review')
+      .should('be.visible');
+  });
+
+  it('should not show the agent mode switch button"', () => {
+    cy.get('smart-camera-web')
+      .shadow()
+      .find('selfie-capture')
+      .invoke('attr', 'show-agent-mode-for-tests', 'false');
+
+    cy.clock();
+    cy.get('smart-camera-web')
+      .shadow()
+      .find('selfie-capture-instructions')
+      .shadow()
+      .find('#allow')
+      .click();
+
+    cy.get('smart-camera-web')
+      .shadow()
+      .find('selfie-capture')
+      .should('be.visible');
+
+    cy.get('smart-camera-web')
+      .shadow()
+      .find('selfie-capture')
+      .shadow()
+      .find('#switch-camera')
+      .should('not.exist');
 
     cy.get('smart-camera-web')
       .shadow()

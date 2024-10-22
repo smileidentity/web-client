c6950bb4be21b56633fe18e0e6f96f923115828c
Barnabas A Nsoh <banasco@gmail.com>
2024-10-22 10:19:09 +0000

    Preview app (#296)
    * add previews
    
    * Add preview app
    
    * link files
    
    * Fix lint
    
    * Downgrade sst due to this issue https://github.com/sst/ion/issues/774
    
    * Update readme
    
    * Add destroy workflow when pr is closed
    
    * Fix destroy deployed preview app workflow name
    
    * Add script to retrieve the app url
    
    * Set the right branch name for deployment
    Share preview app url as comment on PR
    
    * Deploy preview regardless of outcome of other steps
    
    * Sanitize branch name
    
    * Fix sst url after deploy
    
    * fix share preview url
    
    * use $GITHUB_HEAD_REF to get branch name when sharing preivew
    
    * Support deployment when pr is reopened
    
    * use sanitized branch name for preview url sharing
    
    * Remove pr reopened from workflow
    
    * resolve package-lock
    
    * enh: full-stack preview app enhancements (#317)
    
    * refactor: change `SS_STAGE` to `SST_STAGE`
    
    * deps: use latest stable release of sst
    
    sst v3 is now a stable release. this removes the need for the go binary
    install step. npm wins, yet again
    
    * fix: fix lint, prettier issues
    
    * refactor: remove redundant `default_app` directory
    
    ---------
    
    Co-authored-by: Barnabas A Nsoh <banasco@gmail.com>
    
    * Fix lint
    
    * add npm to run deploy preview
    
    * add sst script
    
    ---------
    
    Co-authored-by: tams sokari <tamssokari@users.noreply.github.com>

---
+++ previews/app/routes/products.$productName.tsx
@@ -0,0 +1,179 @@
+import type { LoaderFunctionArgs } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { useState } from 'react';
+import { Resource } from 'sst';
+import products from '~/data/products.json';
+
+declare global {
+  interface Window {
+    SmileIdentity: Function;
+  }
+}
+
+interface InputField {
+  type: 'select' | 'text' | 'url';
+  label: string;
+  name: string;
+  required: boolean;
+}
+
+interface SelectOption {
+  name: string;
+  label: string;
+}
+
+interface SelectInput extends InputField {
+  options: SelectOption[];
+}
+
+interface Product {
+  input_fields: (InputField | SelectInput)[];
+  label: string;
+  name: string;
+  url: string;
+}
+
+interface TokenResults {
+  callback_url: string;
+  document_capture_modes?: string;
+  document_id?: string;
+  partner_id: string;
+  token: string;
+}
+
+const getToken = async (apiUrl: string, body: string | object) => {
+  try {
+    const result = await fetch(apiUrl, {
+      method: 'POST',
+      body: typeof body === 'string' ? body : JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const jsonResponse = await result.json();
+    return jsonResponse;
+  } catch (error) {
+    return error;
+  }
+};
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  const product = products.find((p) => p.url === params.productName);
+  if (product) {
+    return {
+      product,
+      apiUrl: Resource.GetToken.url,
+      appStage: Resource.App.stage,
+    };
+  }
+  return null;
+}
+
+export default function Product() {
+  const { product, apiUrl, appStage } = useLoaderData<{
+    product: Product;
+    apiUrl: string;
+    appStage: string;
+  } | null>(null);
+  const [isGettingToken, setIsGettingToken] = useState<boolean>(false);
+
+  function initializeSdk(config: TokenResults) {
+    if (typeof window.SmileIdentity === 'function' && config) {
+      window.SmileIdentity({
+        ...config,
+        document_ids: [config.document_id],
+        document_capture_modes: (
+          (config.document_capture_modes as string) ?? ''
+        ).split(','),
+        partner_details: {
+          partner_id: config.partner_id,
+          name: 'Demo Account',
+          logo_url: 'https://via.placeholder.com/50/000000/FFFFFF?text=DA',
+          policy_url: 'https://usesmileid.com/privacy-privacy',
+          theme_color: '#000',
+        },
+        onSuccess: () => {},
+        onClose: () => {},
+        onError: () => {},
+      });
+    }
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const body = Object.fromEntries(formData);
+    setIsGettingToken(true);
+    try {
+      const tokenResults = await getToken(apiUrl, body);
+      initializeSdk(tokenResults);
+      return tokenResults;
+    } catch (error) {
+      return new Response(JSON.stringify({ error }), { status: 400 });
+    } finally {
+      setIsGettingToken(false);
+    }
+  }
+
+  return (
+    <>
+      {product === null ? <h1>Unsupported Product</h1> : null}
+
+      {product !== null ? (
+        <>
+          <form onSubmit={handleSubmit}>
+            <fieldset disabled={isGettingToken}>
+              <legend>
+                <h1>{product.label}</h1>
+              </legend>
+
+              <input type="hidden" name="product" value={product.name} />
+              {product.input_fields.map((input, inputIdx) => (
+                <div key={inputIdx}>
+                  {input.type === 'select' ? (
+                    <>
+                      <label htmlFor={`input-${inputIdx}`}>{input.label}</label>
+                      <select
+                        disabled={isGettingToken}
+                        required={input.required}
+                        name={input.name}
+                        id={`input-${inputIdx}`}
+                      >
+                        {(input as SelectInput).options.map(
+                          (option, optionIdx) => (
+                            <option key={optionIdx} value={option.name}>
+                              {option.label}
+                            </option>
+                          ),
+                        )}
+                      </select>
+                    </>
+                  ) : (
+                    <label>
+                      <span>{input.label}</span>
+                      <input
+                        disabled={isGettingToken}
+                        required={input.required}
+                        type={input.type}
+                        name={input.name}
+                      />
+                    </label>
+                  )}
+                </div>
+              ))}
+              <button disabled={isGettingToken} type="submit">
+                Create Preview
+              </button>
+            </fieldset>
+          </form>
+
+          <script
+            src={`https://cdn.smileidentity.com/inline/${
+              appStage.includes('preview/') ? appStage : 'v1'
+            }/js/script.min.js`}
+          ></script>
+        </>
+      ) : null}
+    </>
+  );
+}
---
+++ previews/get_app_url.js
@@ -0,0 +1,3 @@
+import { Resource } from 'sst';
+
+console.log(Resource.PreviewApp.url);

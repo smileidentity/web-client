cff526806820c3cdbf1af5b8db4348ea362aa6d0
Barnabas Nsoh <banasco@gmail.com>
2024-12-16 17:22:07 +0000

    use npm for sst to ensure package version is used

--- previews/sst-env.d.ts
+++ previews/sst-env.d.ts
@@ -5,5 +5,31 @@
 import 'sst';
 export {};
 declare module 'sst' {
-  export interface Resource {}
+  export interface Resource {
+    CallbackUrl: {
+      type: 'sst.sst.Secret';
+      value: string;
+    };
+    GetToken: {
+      name: string;
+      type: 'sst.aws.Function';
+      url: string;
+    };
+    PartnerId: {
+      type: 'sst.sst.Secret';
+      value: string;
+    };
+    PreviewApp: {
+      type: 'sst.aws.Remix';
+      url: string;
+    };
+    SmileIdApiKey: {
+      type: 'sst.sst.Secret';
+      value: string;
+    };
+    SmileIdEnvironment: {
+      type: 'sst.sst.Secret';
+      value: string;
+    };
+  }
 }

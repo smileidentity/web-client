3e4298474dd01773b4158a00f74ab0455978c6fa
Barnabas Nsoh <banasco@gmail.com>
2024-12-16 13:20:22 +0000

    fix lint

--- previews/sst-env.d.ts
+++ previews/sst-env.d.ts
@@ -2,34 +2,34 @@
 /* tslint:disable */
 /* eslint-disable */
 /* deno-fmt-ignore-file */
-import "sst"
-export {}
-declare module "sst" {
+import 'sst';
+export {};
+declare module 'sst' {
   export interface Resource {
-    "CallbackUrl": {
-      "type": "sst.sst.Secret"
-      "value": string
-    }
-    "GetToken": {
-      "name": string
-      "type": "sst.aws.Function"
-      "url": string
-    }
-    "PartnerId": {
-      "type": "sst.sst.Secret"
-      "value": string
-    }
-    "PreviewApp": {
-      "type": "sst.aws.Remix"
-      "url": string
-    }
-    "SmileIdApiKey": {
-      "type": "sst.sst.Secret"
-      "value": string
-    }
-    "SmileIdEnvironment": {
-      "type": "sst.sst.Secret"
-      "value": string
-    }
+    CallbackUrl: {
+      type: 'sst.sst.Secret';
+      value: string;
+    };
+    GetToken: {
+      name: string;
+      type: 'sst.aws.Function';
+      url: string;
+    };
+    PartnerId: {
+      type: 'sst.sst.Secret';
+      value: string;
+    };
+    PreviewApp: {
+      type: 'sst.aws.Remix';
+      url: string;
+    };
+    SmileIdApiKey: {
+      type: 'sst.sst.Secret';
+      value: string;
+    };
+    SmileIdEnvironment: {
+      type: 'sst.sst.Secret';
+      value: string;
+    };
   }
 }

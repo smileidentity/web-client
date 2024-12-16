bd897d558f50672cd932bcc7a7efc1da19090c80
Barnabas Nsoh <banasco@gmail.com>
2024-12-16 16:53:00 +0000

    use npx in place of npm run

--- previews/sst-env.d.ts
+++ previews/sst-env.d.ts
@@ -5,31 +5,5 @@
 import 'sst';
 export {};
 declare module 'sst' {
-  export interface Resource {
-    CallbackUrl: {
-      type: 'sst.sst.Secret';
-      value: string;
-    };
-    GetToken: {
-      name: string;
-      type: 'sst.aws.Function';
-      url: string;
-    };
-    PartnerId: {
-      type: 'sst.sst.Secret';
-      value: string;
-    };
-    PreviewApp: {
-      type: 'sst.aws.Remix';
-      url: string;
-    };
-    SmileIdApiKey: {
-      type: 'sst.sst.Secret';
-      value: string;
-    };
-    SmileIdEnvironment: {
-      type: 'sst.sst.Secret';
-      value: string;
-    };
-  }
+  export interface Resource {}
 }

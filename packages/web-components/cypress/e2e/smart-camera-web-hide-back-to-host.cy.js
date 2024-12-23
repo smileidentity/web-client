95e3b98870687b7491ad2e75665e932e60646a60
Barnabas Nsoh <banasco@gmail.com>
2024-12-23 15:00:50 +0000

    fix lint

--- packages/web-components/cypress/e2e/smart-camera-web-hide-back-to-host.cy.js
+++ packages/web-components/cypress/e2e/smart-camera-web-hide-back-to-host.cy.js
@@ -31,31 +31,31 @@ describe('SmartCameraWeb', () => {
 
   it('hides back exit and cancel button when `hide-back-to-host` attribute is passed', () => {
     cy.get('smart-camera-web')
-    .invoke('attr', 'hide-back-to-host', 'true')
-    .should('have.attr', 'hide-back-to-host', 'true');
+      .invoke('attr', 'hide-back-to-host', 'true')
+      .should('have.attr', 'hide-back-to-host', 'true');
 
     cy.get('smart-camera-web')
-    .shadow()
-    .find('selfie-capture-instructions')
-    .should('be.visible');
-  cy.get('smart-camera-web')
-    .shadow()
-    .find('selfie-capture-instructions')
-    .shadow()
-    .should('contain.text', "Next, we'll take a quick selfie");
-  cy.get('smart-camera-web')
-    .shadow()
-    .find('selfie-capture-instructions')
-    .shadow()
-    .find('smileid-navigation')
-    .shadow()
-    .get('.back-button')
-    .should('not.exist');
-  cy.get('smart-camera-web')
-    .shadow()
-    .find('selfie-capture-instructions')
-    .shadow()
-    .get('#cancel')
-    .should('not.exist');
+      .shadow()
+      .find('selfie-capture-instructions')
+      .should('be.visible');
+    cy.get('smart-camera-web')
+      .shadow()
+      .find('selfie-capture-instructions')
+      .shadow()
+      .should('contain.text', "Next, we'll take a quick selfie");
+    cy.get('smart-camera-web')
+      .shadow()
+      .find('selfie-capture-instructions')
+      .shadow()
+      .find('smileid-navigation')
+      .shadow()
+      .get('.back-button')
+      .should('not.exist');
+    cy.get('smart-camera-web')
+      .shadow()
+      .find('selfie-capture-instructions')
+      .shadow()
+      .get('#cancel')
+      .should('not.exist');
   });
 });

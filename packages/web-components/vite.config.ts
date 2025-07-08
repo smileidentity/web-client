c2f6f588fd8b10ac33455ebb9d1520744a48b8d8
Khalifa Lame <khalibloo@gmail.com>
2025-07-08 17:37:32 +0400

    fix: avoid clearing iife build

--- packages/web-components/vite.config.ts
+++ packages/web-components/vite.config.ts
@@ -89,7 +89,7 @@ export default defineConfig(({ mode }) => {
     },
     sourcemap: isProduction,
     minify: isProduction,
-    emptyOutDir: true,
+    emptyOutDir: buildFormat === 'esm',
   };
 
   const iifeBuildConfig = {
@@ -112,7 +112,7 @@ export default defineConfig(({ mode }) => {
     sourcemap: isProduction,
     minify: isProduction,
     outDir: 'dist',
-    emptyOutDir: buildFormat === 'iife', // Only clear on IIFE build
+    emptyOutDir: false,
   };
 
   return {

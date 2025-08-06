00d6dccf1d37aa3ad8edbf1905d1845ce542f3cd
Khalifa Lame <khalibloo@gmail.com>
2025-08-06 19:23:07 +0400

    ci: use browser flags in cypress for camera/video

--- packages/embed/cypress.config.js
+++ packages/embed/cypress.config.js
@@ -8,6 +8,26 @@ const config = defineConfig({
       runMode: 2,
       openMode: 0,
     },
+    setupNodeEvents(on) {
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.family === 'chromium') {
+          // camera and video args
+          launchOptions.args.push('--use-fake-ui-for-media-stream');
+          launchOptions.args.push('--use-fake-device-for-media-stream');
+          launchOptions.args.push('--autoplay-policy=no-user-gesture-required');
+
+          // CI-specific args
+          launchOptions.args.push('--disable-dev-shm-usage'); // Helps with CI memory issues
+          launchOptions.args.push('--no-sandbox');
+
+          // additional media-related args that seem to improve stability
+          launchOptions.args.push('--disable-web-security');
+          launchOptions.args.push('--allow-running-insecure-content');
+          launchOptions.args.push('--disable-features=VizDisplayCompositor');
+        }
+        return launchOptions;
+      });
+    },
   },
 });
 

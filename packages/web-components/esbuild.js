8ffbb02283aab54870ea4accc19218e83fffaa17
Barnabas Nsoh <banasco@gmail.com>
2025-08-06 13:05:37 +0000

    build for android 7.1

--- packages/web-components/esbuild.js
+++ packages/web-components/esbuild.js
@@ -153,7 +153,7 @@ const buildIife = () =>
     ...buildOptions,
     format: 'iife',
     outdir: `${buildDir}`,
-    target: ['chrome63', 'firefox57', 'safari11', 'edge79'], // Support Android 8.1+ Chrome WebView 63+
+        target: ['chrome55', 'firefox57', 'safari11', 'edge79'], // Support Android 7.1.1+ Chrome WebView 55+
   });
 
 /**

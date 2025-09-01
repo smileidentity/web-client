db9c3f12ff6cabad3b55fcd3d7fc501e46f46228
Khalifa Lame <khalibloo@gmail.com>
2025-09-01 18:05:58 +0400

    Revert "test: also check that strings are not empty"
    This reverts commit 02430a8694e8b209e1f0049c6661c80c59cb27f1.

--- packages/embed/cypress/tests/id-selection.cy.cjs
+++ packages/embed/cypress/tests/id-selection.cy.cjs
@@ -91,17 +91,13 @@ describe('No ID Selection', () => {
       // Check request headers
       const headers = interception.request.headers;
       expect(headers).to.have.property('smileid-request-mac');
-      expect(headers['smileid-request-mac'])
-        .to.be.a('string')
-        .and.not.be.empty();
+      expect(headers['smileid-request-mac']).to.be.a('string');
       expect(headers).to.have.property('smileid-request-timestamp');
       expect(headers['smileid-request-timestamp']).to.match(
         /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/,
       );
       expect(headers).to.have.property('smileid-partner-id');
-      expect(headers['smileid-partner-id'])
-        .to.be.a('string')
-        .and.not.be.empty();
+      expect(headers['smileid-partner-id']).to.be.a('string');
     });
 
     cy.wait('@successfulUpload');

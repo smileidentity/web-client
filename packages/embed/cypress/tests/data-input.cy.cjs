51029a19e1340666470f780f39bed1a406039e9b
Barnabas Nsoh <banasco@gmail.com>
2025-12-23 15:00:28 +0000

    update test

--- packages/embed/cypress/tests/data-input.cy.cjs
+++ packages/embed/cypress/tests/data-input.cy.cjs
@@ -45,7 +45,7 @@ describe('id-info screen', () => {
 
     cy.getIFrameBody()
       .find('#id_number-hint')
-      .should('contain', 'Id number is invalid');
+      .should('contain', 'ID Number is invalid');
   });
 
   it('should progress when input is valid', () => {
